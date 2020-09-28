4d639a99ddd0331221619032818765b5f4b7028c
lsshawn <coffee.or.tea88@gmail.com>
2020-09-28 18:57:15 +0800

    setup vue-router and vuex'

--- vue-dashboard/src/App.vue
+++ vue-dashboard/src/App.vue
@@ -4,6 +4,14 @@
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>Dashboard</v-toolbar-title>
     </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
